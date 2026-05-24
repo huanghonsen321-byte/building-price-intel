d5c229f0f2f1219f241909f8691ce5d795fdb3f7
huanghonsen321-byte <huanghonsen321@gmail.com>
2026-05-24 08:28:33 +0800

    refactor: remove crawler-related modules and routes (#43)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,6 @@
 from app.models.attachment import BidAttachment
 from app.models.bid import ScaffoldBidCase, ScaffoldPriceReference
 from app.models.crawl import BidRawDocument, CrawlSource, CrawlTask
-from app.models.crawl_orchestrator import CrawlRun, CrawlRunSource
-from app.models.managed_browser_run import ManagedBrowserRun
 from app.models.notification import NotificationLog
 from app.models.price import PriceDaily
 from app.models.review import ReviewTask
@@ -10,11 +8,8 @@ from app.models.review import ReviewTask
 __all__ = [
     "BidAttachment",
     "BidRawDocument",
-    "CrawlRun",
-    "CrawlRunSource",
     "CrawlSource",
     "CrawlTask",
-    "ManagedBrowserRun",
     "NotificationLog",
     "PriceDaily",
     "ReviewTask",
